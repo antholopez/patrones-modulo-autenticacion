77c6d5fb4a06c335b6f1025e3d70a175e1f072e0
Anthony Lopez <anthodev1195@gmail.com>
2021-01-24 22:52:35 -0500

    fix ok

--- src/database.ts
+++ src/database.ts
@@ -24,7 +24,7 @@ export class Database {
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      entities: [String(process.env.DB_ENTTIES)],
+      entities: ["dist/entities/**/*.{js,ts}"],
       logging: true,
     });
   }
